52adbcb663502dfe1f17cdbdc087a9ef5e2f1ad5
cybernahid-dev <githubnahid@gmail.com>
2026-01-13 23:11:26 +0600

    navigation bug fixed

--- app/src/main/java/com/aeoncorex/streamx/navigation/AppNavigation.kt
+++ app/src/main/java/com/aeoncorex/streamx/navigation/AppNavigation.kt
@@ -1,9 +1,11 @@
 package com.aeoncorex.streamx.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.aeoncorex.streamx.ui.account.AccountScreen
 import com.aeoncorex.streamx.ui.auth.AuthScreen
 import com.aeoncorex.streamx.ui.main.MainScreen
@@ -15,22 +17,27 @@ import com.aeoncorex.streamx.ui.privacy.PrivacyPolicyScreen
 import com.aeoncorex.streamx.ui.about.AboutScreen
 import com.aeoncorex.streamx.ui.music.MusicScreen
 import com.aeoncorex.streamx.ui.music.MusicPlayerScreen
+import com.aeoncorex.streamx.ui.player.PlayerScreen // ইমপোর্ট করা হলো
+import com.aeoncorex.streamx.ui.copyright.CopyrightScreen // নিশ্চিত করুন এই ফাইলটি আছে
 
 @Composable
 fun AppNavigation(themeViewModel: ThemeViewModel) {
     val navController = rememberNavController()
+    
     NavHost(navController = navController, startDestination = "splash") {
         composable("splash") { SplashScreen(navController) }
         composable("auth") { AuthScreen(navController) }
         composable("home") { MainScreen(navController) }
         
-        // ভিডিও প্লেয়ার রুট
+        // ভিডিও প্লেয়ার রুট - NavType এবং navArgument ফিক্স করা হয়েছে
         composable(
             route = "player/{encodedUrl}",
-            arguments = listOf(navArgument("encodedUrl") { type = NavType.StringType })
+            arguments = listOf(
+                navArgument("encodedUrl") { type = NavType.StringType }
+            )
         ) { backStackEntry ->
             val encodedUrl = backStackEntry.arguments?.getString("encodedUrl") ?: ""
-            PlayerScreen(navController = navController, encodedUrl = encodedUrl)
+            PlayerScreen(navController = navController, encodedUrl = encodedUrl, encodedTitle = "Streaming...") 
         }
         
         // --- MUSIC SECTION ROUTES ---
