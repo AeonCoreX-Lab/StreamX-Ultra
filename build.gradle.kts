7970cd48e56a9bec43b83018ee0fa1ee38265606
Nahid Hasan <githubnahid@gmail.com>
2026-04-02 17:03:19 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,17 @@
 // Top-level build.gradle.kts
 plugins {
-    // Android Gradle Plugin 9.0.1 (Android 16 Ready)
+    // Android Gradle Plugin 9.0.1 (compatible with Gradle 9.4.1)
     id("com.android.application") version "9.0.1" apply false
     id("com.android.library") version "9.0.1" apply false
-    
-    // Kotlin 2.0.21 (Latest with K2 compiler)
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 
-    // *** Kotlin 2.0+ Compose Compiler Plugin ***
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
-    
+    // Kotlin 2.1.20 (latest stable, supports Gradle 9.x)
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+
+    // Compose compiler plugin (same version as Kotlin)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+
     // Google Services
     id("com.google.gms.google-services") version "4.4.2" apply false
-    
-    // ❌ REMOVED: Mozilla Rust Plugin (We are using cargo-ndk instead)
-    // id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3" apply false
 }
 
 tasks.register<Delete>("clean") {
